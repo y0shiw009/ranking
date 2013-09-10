c51ab848b458c43f2f687f1c9a489950f591480e
watanabe_yoshiyuki <watanabe_yoshiyuki@cyberagent.co.jp>
2013-09-10 17:48:08 +0900

    コネクションをキャッシュ
    Signed-off-by: watanabe_yoshiyuki <watanabe_yoshiyuki@cyberagent.co.jp>

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,18 +9,24 @@ import org.apache.hadoop.hbase.client.Put
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.hbase.client.Get
 import org.apache.hadoop.hbase.client.HBaseAdmin
+import org.apache.hadoop.hbase.client.HTablePool
+import org.apache.hadoop.hbase.client.HConnectionManager
+import org.apache.hadoop.hbase.client.HTableInterface
 
 object Application extends Controller {
 
-    def index = Action {
-        println("debug01")
-        val conf: Configuration = HBaseConfiguration.create();
+    lazy val conf = {
+        val conf = HBaseConfiguration.create();
         conf.set("hbase.zookeeper.quorum", "ec2-54-250-225-111.ap-northeast-1.compute.amazonaws.com");
         conf.set("hbase.zookeeper.property.clientPort", "2181");
+        conf
+    }
 
-        println("debug011")
+    lazy val connection = HConnectionManager.createConnection(conf);
 
-        val table = new HTable(conf, "test")
+    def index = Action {
+        println("debug01")
+        val table: HTableInterface = connection.getTable("test");
 
         println("debug02")
         val result = table.get(new Get(Bytes.toBytes("row")))
@@ -29,6 +35,7 @@ object Application extends Controller {
         val value = result.getColumnLatest(Bytes.toBytes("family1"), Bytes.toBytes("qualifier"))
 
         println(Bytes.toString(value.getValue()))
+        table.close();
         Ok(views.html.index("Your new application is ready."))
     }
 
