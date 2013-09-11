57d659a4cebf2a6203891d06cd30112a91ad4008
watanabe_yoshiyuki <watanabe_yoshiyuki@cyberagent.co.jp>
2013-09-11 19:14:58 +0900

    Signed-off-by: watanabe_yoshiyuki <watanabe_yoshiyuki@cyberagent.co.jp>

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,17 +1,15 @@
 package controllers
 
-import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.HBaseConfiguration
-import play.api.mvc.Action
-import play.api.mvc.Controller
-import org.apache.hadoop.hbase.client.HTable
-import org.apache.hadoop.hbase.client.Put
-import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.hbase.client.Get
-import org.apache.hadoop.hbase.client.HBaseAdmin
-import org.apache.hadoop.hbase.client.HTablePool
 import org.apache.hadoop.hbase.client.HConnectionManager
 import org.apache.hadoop.hbase.client.HTableInterface
+import org.apache.hadoop.hbase.util.Bytes
+
+import play.api.libs.json.Json
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import play.api.mvc.Result
 
 object Application extends Controller {
 
@@ -23,6 +21,25 @@ object Application extends Controller {
     }
 
     val connection = HConnectionManager.createConnection(conf);
+
+    def get(asid: String, evt: String) = Action {
+        val table: HTableInterface = connection.getTable("ranking");
+        val result = table.get(new Get(Bytes.toBytes(asid)))
+        val value = result.getColumnLatest(Bytes.toBytes("data"), Bytes.toBytes("cid=gf~evt=g1"))
+        toJsonResult(Ok(Json.toJson(Map("asid" -> asid, "evt" -> Bytes.toString(value.getValue())))))
+    }
+
+    def inc(asid: String, evt: String, pnt: String) = Action {
+        val table: HTableInterface = connection.getTable("ranking");
+        val result = table.get(new Get(Bytes.toBytes(asid)))
+        val value = result.getColumnLatest(Bytes.toBytes("data"), Bytes.toBytes("cid=gf~evt=g1"))
+        toJsonResult(Ok(Json.toJson(Map("asid" -> asid, "evt" -> Bytes.toString(value.getValue())))))
+    }
+
+    private def toJsonResult(result: Result): Result = {
+        result.withHeaders("Accept" -> "application/json",
+            "Content-Type" -> "application/json; charset=utf-8")
+    }
 
     def index = Action {
         println("debug01")
